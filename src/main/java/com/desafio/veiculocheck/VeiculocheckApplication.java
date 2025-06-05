e517e2d7dd0f0a1379236786dbb577d631fd69e6
kauefilgueiras <kauefilgueiras@gmail.com>
2025-06-05 20:23:33 -0300

    first commit

---
+++ src/main/java/com/desafio/veiculocheck/VeiculocheckApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.veiculocheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculocheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculocheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/veiculocheck/VeiculocheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.veiculocheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculocheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
